b78c272d723f6695d340dc32aff719d4047a0ceb
Yama-Kosuke <rag.rag.rag.kosuke0527@gmail.com>
2021-01-14 05:51:08 +0900

    サイト上にPokemonの画像と名前を表示させました。

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,44 @@
 import Head from "next/Head";
 import Layout from "../components/Layout";
-
-export default function Home(pokemon) {
+import Link from "next/Link";
+export default function Home({ pokemon }) {
   console.log(pokemon)
   return (
     <Layout title="NextJS Pokedex">
-      <h1 className="text-4xl mb-8 text-center">NextJS Pokedex</h1>
+      <h1 className="text-4xl mb-8 text-center ">The Nextjs Pokedex</h1>
+      <ul>
+        {pokemon.map((pokeman, index) => (
+          <li key={index}>
+            <Link href={`/pokemon?id=${index + 1}`}>
+              <a className="border p-4 border-grey my-2 hover:shadow-md capitalize flex items-center text-lg bg-gray-200 rounded-md">
+                <img
+                  src={pokeman.image}
+                  alt={pokeman.name}
+                  className="w-20 h-20 mr-3"
+                />
+                <span className="mr-2 font-bold">{index + 1}.</span>
+                {pokeman.name}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
 
 export async function getStaticProps(context) {
   try {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1000");
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=898");
     const { results } = await res.json();
-    const pokemon = results.map((result, index) => {
+    const pokemon = results.map((pokeman, index) => {
       const paddedIndex = ("00" + (index + 1)).slice(-3);
+
       const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`;
-      return {
-        ...result,
-        image,
-      };
+      return { ...pokeman, image };
     });
     return {
-      props: {pokemon},
+      props: { pokemon },
     };
   } catch (err) {
     console.error(err);
