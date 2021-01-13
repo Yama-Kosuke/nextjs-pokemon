4433b4ea18154be3a880d0bbe520e849b106df84
Yama-Kosuke <rag.rag.rag.kosuke0527@gmail.com>
2021-01-14 05:28:48 +0900

    PokemonAPIを叩き、console.logで実際にAPIデータを取得できるところまで確認

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
-import Head from "next/head";
+import Head from "next/Head";
+import Layout from "../components/Layout";
 
-export default function Home() {
+export default function Home(pokemon) {
+  console.log(pokemon)
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main></main>
-    </div>
+    <Layout title="NextJS Pokedex">
+      <h1 className="text-4xl mb-8 text-center">NextJS Pokedex</h1>
+    </Layout>
   );
+}
+
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1000");
+    const { results } = await res.json();
+    const pokemon = results.map((result, index) => {
+      const paddedIndex = ("00" + (index + 1)).slice(-3);
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`;
+      return {
+        ...result,
+        image,
+      };
+    });
+    return {
+      props: {pokemon},
+    };
+  } catch (err) {
+    console.error(err);
+  }
 }
